5c241095eada9cab02ce9eb3dfbc9b550a1af366
Yi Zhou <zhouyi@biomap.com>
2025-08-04 21:45:18 +0800

    fix(numbering): avoid bad behavior in AntPack of changing the cwd

--- src/antid/utils/patch_antpack.py
+++ src/antid/utils/patch_antpack.py
@@ -8,19 +8,78 @@ the class is instantiated, improving performance in applications
 where the class is used frequently.
 """
 
+import gzip
 from pathlib import Path
 
 import antpack
 import numpy as np
 from antpack.antpack_cpp_ext import SingleChainAnnotatorCpp, VJMatchCounter
 from antpack.numbering_tools.cterm_finder import _load_nterm_kmers
-from antpack.utilities.vj_utilities import load_vj_gene_consensus_db
 
+from antid.utils import check_path
+
+
+def load_vj_gene_consensus_db(database_path: str | Path, database: str = "imgt"):
+    """Patch of antpack.utilities.vj_utilities.load_vh_gene_consensus_db.
+
+    Args:
+        current_dir: The current user directory.
+        database_path: The location of the consensus databases.
+        database: The name of the database to load (default is "imgt").
+    """
+    vj_names, vj_seqs, retrieved_dates = {}, {}, {}
+
+    try:
+        db_path = check_path(database_path, exists=True, is_dir=True)
+        db_files = [
+            f for f in db_path.glob("*.fa.gz") if f.stem.split("_")[1] == database
+        ]
+        for db_file in db_files:
+            fname = db_file.name.split(".")[0]
+            species, _, receptor, retrieved_date = fname.split("_")
+            if species not in retrieved_dates:
+                retrieved_dates[species] = {}
+
+            retrieved_dates[species][receptor] = retrieved_date
+
+            # We avoid using Biopython's SeqIO (since it introduces an additional
+            # unnecessary dependency). Since we wrote the db files, we know
+            # how they are formatted -- one aa seq for each gene name -- and
+            # can use a very simple procedure here which is not applicable to all
+            # fasta files.
+            with gzip.open(db_file, "rt", encoding="utf-8") as f:
+                sequences, names = [], []
+
+                for line in f:
+                    if line.startswith(">"):
+                        description = line.strip()[1:]
+                    else:
+                        aa_seq = line.strip()
+                        sequences.append(aa_seq)
+                        names.append(description)
+
+            key = f"{species}_{receptor}"
+            if key in vj_names:
+                raise RuntimeError(
+                    f"Duplicate db files found for {species}, {receptor}."
+                )
+
+            vj_names[key] = names
+            vj_seqs[key] = sequences
+
+    except Exception as e:
+        raise RuntimeError(
+            "The consensus data for the package either has been deleted or "
+            "moved or was never properly installed."
+        ) from e
+
+    return vj_names, vj_seqs, retrieved_dates
+
+
+# Global variables to avoid loading the database multiple times
 antpack_path = Path(antpack.__file__).parent.resolve()
 db_path = antpack_path / "vj_tools" / "consensus_data"
-vj_names, vj_seqs, retrieved_dates = load_vj_gene_consensus_db(
-    antpack_path, db_path, "imgt"
-)
+vj_names, vj_seqs, retrieved_dates = load_vj_gene_consensus_db(db_path, "imgt")
 ig_aligner_consensus_path = antpack_path / "numbering_tools" / "consensus_data"
 blosum_matrix = np.load(
     ig_aligner_consensus_path / "mabs" / "blosum_matrix.npy"
