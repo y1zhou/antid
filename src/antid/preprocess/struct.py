89b305002abc598af91222afa2a27e58e83bf19b
y1zhou <zhou.zy.yi@gmail.com>
2025-12-16 15:23:37 +0800

    feat(preprocess): add method to assign antibody-numbered residue indices to structure file

--- src/antid/preprocess/struct.py
+++ src/antid/preprocess/struct.py
@@ -6,6 +6,7 @@ import gemmi
 import polars as pl
 
 from antid.io.seq import AA3TO1
+from antid.numbering.antibody import ValidSchemesType, number_ab_seq
 from antid.utils import check_path
 
 
@@ -106,6 +107,7 @@ def align_ref_seq_to_struct(
             )
         )
 
+    # TODO: replace with https://gemmi.readthedocs.io/en/stable/analysis.html#sequence-alignment
     from Bio import Align
 
     aligner = Align.PairwiseAligner(scoring="blastp")
@@ -155,6 +157,12 @@ def standardize_struct_file(
     output_file: str | Path,
     restart_each_chain: bool = True,
     chain_mapping: dict[str, str] | None = None,
+    remove_waters: bool = True,
+    remove_ligands_and_waters: bool = False,
+    # Antibody-specific
+    vh_chain: str | None = None,
+    vl_chain: str | None = None,
+    numbering_scheme: ValidSchemesType | None = None,
 ) -> pl.DataFrame:
     """Renumber PDB and mmCIF coordinate files with serial residue indices.
 
@@ -169,11 +177,24 @@ def standardize_struct_file(
         output_file: Path to the output PDB file.
         restart_each_chain: If True, residue indices will restart from 1 for each chain.
             If False, residue indices will be continuous across the entire structure.
+            Ignored if `vh_chain` or `vl_chain` is provided.
         chain_mapping: Optional mapping of old chain IDs to new chain IDs. If provided,
             the chain IDs in the output PDB file will be replaced according to this mapping.
             The output file will also *only* contain the chains in the mapping. If not
             provided, all chains in the input file will be kept, and only residue
-            renumbering will be performed.
+            renumbering will be performed. If ``vh_chain`` or ``vl_chain`` is provided,
+            they should use the new chain IDs after renaming.
+        remove_waters: If True, water molecules will be removed from the structure.
+        remove_ligands_and_waters: If True, all non-protein residues (including ligands
+            and water molecules) will be removed from the structure.
+        vh_chain: Heavy chain ID. If provided, and `numbering_scheme` is also provided,
+            the heavy chain will be renumbered according to the specified scheme. Residues
+            that are not in the Fv region will be removed.
+        vl_chain: Light chain ID. If provided, and `numbering_scheme` is also provided,
+            the light chain will be renumbered according to the specified scheme. Residues
+            that are not in the Fv region will be removed.
+        numbering_scheme: Antibody numbering scheme to use for renumbering the heavy
+            and light chains.
 
     Returns:
         A polars DataFrame with:
@@ -193,6 +214,10 @@ def standardize_struct_file(
     st.setup_entities()
     st.remove_alternative_conformations()
     st.standardize_crystal_frame()
+    if remove_waters:
+        st.remove_waters()
+    if remove_ligands_and_waters:
+        st.remove_ligands_and_waters()
 
     # Rename chains if a mapping is provided
     model = st[0]
@@ -213,31 +238,85 @@ def standardize_struct_file(
     res_map: list[tuple[int, str, int, str, str, int, str]] = []
     prev_resi = 0
 
+    if (vh_chain is not None) or (vl_chain is not None):
+        restart_each_chain = False  # override when using antibody numbering
+    chains_to_number = []
+    if vh_chain is not None:
+        chains_to_number.append(vh_chain)
+    if vl_chain is not None:
+        chains_to_number.append(vl_chain)
+
     for chain in model:
-        # TODO: generate residue IDs using antibody numbering
         if chain.name not in model_chains_rev:
             model.remove_chain(chain.name)
             continue
 
         old_chain_id = model_chains_rev[chain.name]
-        resi = 1 if restart_each_chain else prev_resi + 1
-        for res in chain:
-            res_map.append(
-                (
-                    model.num,
-                    old_chain_id,
-                    res.seqid.num,
-                    res.seqid.icode,
-                    chain.name,
-                    resi,
-                    " ",
-                )
-            )
-            res.seqid.num = resi
-            res.seqid.icode = " "
-            resi += 1
 
-        prev_resi = resi
+        if chain.name in chains_to_number:
+            if numbering_scheme is None:
+                raise ValueError(
+                    "If `vh_chain` or `vl_chain` is provided, "
+                    "`numbering_scheme` must also be provided."
+                )
+            chain_seq = gemmi.one_letter_code([res.name for res in chain])
+            numbered_seq = number_ab_seq(chain_seq, numbering_scheme)
+            pos_df = numbered_seq.position.with_columns(
+                (pl.col("idx") - pl.lit(1)).alias("idx"),
+                pl.col("numbered_pos")
+                .str.extract(r"^(\d+)")
+                .cast(pl.Int32)
+                .alias("resi"),
+                pl.col("numbered_pos")
+                .str.replace(r"^\d+", "")
+                .replace("", " ")
+                .alias("icode"),
+            )
+            res_idx_to_remove = (
+                pos_df.filter(pl.col("resi").is_null())
+                .get_column("idx")
+                .sort(descending=True)
+            )
+
+            for res, r in zip(chain, pos_df.iter_rows(named=True), strict=True):
+                if r["idx"] in res_idx_to_remove:
+                    continue
+                res_map.append(
+                    (  # pyrefly: ignore[bad-argument-type]
+                        model.num,
+                        old_chain_id,
+                        res.seqid.num,
+                        res.seqid.icode,
+                        chain.name,
+                        r["resi"],
+                        r["icode"],
+                    )
+                )
+                res.seqid.num = r["resi"]
+                res.seqid.icode = r["icode"]
+
+            for i in res_idx_to_remove:
+                del chain[i]
+
+        else:
+            resi = 1 if restart_each_chain else prev_resi + 1
+            for res in chain:
+                res_map.append(
+                    (  # pyrefly: ignore[bad-argument-type]
+                        model.num,
+                        old_chain_id,
+                        res.seqid.num,
+                        res.seqid.icode,
+                        chain.name,
+                        resi,
+                        " ",
+                    )
+                )
+                res.seqid.num = resi
+                res.seqid.icode = " "
+                resi += 1
+
+            prev_resi = resi
 
     st.remove_empty_chains()
     st.assign_serial_numbers(numbered_ter=True)
